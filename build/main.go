e5b111881cf5a47098c39939f8d6514acc718a58
Ben Hale <bhale@pivotal.io>
2018-11-30 13:18:34 -0800

    Dependency Updates
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- build/main.go
+++ build/main.go
@@ -42,7 +42,7 @@ func main() {
 }
 
 func b(build buildPkg.Build) (int, error) {
-	build.Logger.FirstLine(build.Logger.PrettyVersion(build.Buildpack))
+	build.Logger.FirstLine(build.Logger.PrettyIdentity(build.Buildpack))
 
 	if jdk, ok, err := jdkPkg.NewJDK(build); err != nil {
 		return build.Failure(102), err
