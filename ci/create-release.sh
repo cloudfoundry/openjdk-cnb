1b490cfb3bba92d5db75a9da935aecd07a00ea78
Ben Hale <bhale@pivotal.io>
2018-11-29 20:42:33 -0800

    create-release.sh script
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ ci/create-release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+RELEASE=$1
+SNAPSHOT=$2
+
+update_version() {
+  awk "!x{x=sub(\"version = \\\"1.0.0-BUILD-SNAPSHOT\\\"\",\"version = \\\"${1}\\\"\")}7" buildpack.toml > tmp
+  mv tmp buildpack.toml
+}
+
+git branch -f release
+git checkout release
+update_version $RELEASE
+git add .
+git commit --message "v$RELEASE Release"
+git tag -s v$RELEASE -m "v$RELEASE"
+
+git master
+update_version $SNAPSHOT
+git add .
+git commit --message "v$SNAPSHOT Development"
