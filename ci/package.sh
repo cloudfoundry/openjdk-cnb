5b5500ff94d817a52bc08782971689d8288f316a
Ben Hale <bhale@pivotal.io>
2018-12-04 17:49:01 -0500

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/package.sh
+++ ci/package.sh
@@ -8,16 +8,18 @@ if [[ -d $PWD/go-module-cache && ! -d ${GOPATH}/pkg/mod ]]; then
 fi
 
 PACKAGE_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'package')
-TARGET_DIR="${PWD}/artifactory"
+ARTIFACTORY_DIR="${PWD}/artifactory"
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 go build -ldflags='-s -w' -o bin/package github.com/cloudfoundry/libcfbuildpack/packager
 bin/package ${PACKAGE_DIR}
 
+cd ${PACKAGE_DIR}
+
 ID=$(sed -n 's|id = \"\(.*\)\"|\1|p' buildpack.toml | head -n1)
 VERSION=$(sed -n 's|version = \"\(.*\)\"|\1|p' buildpack.toml | head -n1)
+TARGET="${ARTIFACTORY_DIR}/$(echo ${ID} | sed 's|\.|/|g')/${ID}/${VERSION}/${ID}-$(echo ${VERSION} | sed "s|SNAPSHOT|$(date '+%Y%m%d.%H%M%S')-1|").tgz"
 
-cd ${PACKAGE_DIR}
-mkdir -p ${TARGET_DIR}
-tar czf "${TARGET_DIR}/$(echo ${ID} | sed 's|\.|/|g')/${ID}/${VERSION}/${ID}-$(echo ${VERSION} | sed "s|SNAPSHOT|$(date '+%Y%m%d.%H%M%S')-1|").tgz" *
+mkdir -p $(dirname ${TARGET})
+tar czf ${TARGET} *
