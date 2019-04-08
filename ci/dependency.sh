39f4bf4e368bdb5db00de1cd40181b97769d1d6d
Ben Hale <bhale@pivotal.io>
2019-04-08 08:36:48 -0700

    Dependency Script Improvements
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/dependency.sh
+++ ci/dependency.sh
@@ -7,6 +7,10 @@ if [[ -d $PWD/go-module-cache && ! -d ${GOPATH}/pkg/mod ]]; then
   ln -s $PWD/go-module-cache ${GOPATH}/pkg/mod
 fi
 
+commit() {
+  git commit -a -m "Dependency Upgrade: $1 $2"
+}
+
 version() {
   local PATTERN="([0-9]+)\.([0-9]+)\.([0-9]+)-?(.*)"
 
@@ -27,14 +31,25 @@ version() {
 }
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
+git config --local user.name "$GIT_USER_NAME"
+git config --local user.email ${GIT_USER_EMAIL}
 
 go build -ldflags='-s -w' -o bin/dependency github.com/cloudfoundry/libcfbuildpack/dependency
 
 bin/dependency openjdk-jre "1\.8\.[\d]+"  $(version ../openjdk-jre-8/version)  $(cat ../openjdk-jre-8/uri)  $(cat ../openjdk-jre-8/sha256)
+commit openjdk-jre $(cat ../openjdk-jre-8/version)
+
 bin/dependency openjdk-jdk "1\.8\.[\d]+"  $(version ../openjdk-jdk-8/version)  $(cat ../openjdk-jdk-8/uri)  $(cat ../openjdk-jdk-8/sha256)
+commit openjdk-jdk $(cat ../openjdk-jdk-8/version)
 
 bin/dependency openjdk-jre "11\.0\.[\d]+" $(version ../openjdk-jre-11/version) $(cat ../openjdk-jre-11/uri) $(cat ../openjdk-jre-11/sha256)
+commit openjdk-jre $(cat ../openjdk-jre-11/version)
+
 bin/dependency openjdk-jdk "11\.0\.[\d]+" $(version ../openjdk-jdk-11/version) $(cat ../openjdk-jdk-11/uri) $(cat ../openjdk-jdk-11/sha256)
+commit openjdk-jdk $(cat ../openjdk-jdk-11/version)
 
 bin/dependency openjdk-jre "12\.0\.[\d]+" $(version ../openjdk-jre-12/version) $(cat ../openjdk-jre-12/uri) $(cat ../openjdk-jre-12/sha256)
+commit openjdk-jre $(cat ../openjdk-jre-12/version)
+
 bin/dependency openjdk-jdk "12\.0\.[\d]+" $(version ../openjdk-jdk-12/version) $(cat ../openjdk-jdk-12/uri) $(cat ../openjdk-jdk-12/sha256)
+commit openjdk-jdk $(cat ../openjdk-jdk-12/version)
