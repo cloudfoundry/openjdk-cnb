3242782c8331a75dd5e31e1490a034f8f0ac6567
Ben Hale <bhale@pivotal.io>
2019-05-01 16:35:13 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -10,14 +10,12 @@ update_version() {
   mv tmp buildpack.toml
 }
 
-git branch -f release
-git checkout release
 update_version ${RELEASE}
 git add .
 git commit --message "v${RELEASE} Release"
 git tag -s v${RELEASE} -m "v${RELEASE}"
 
-git master
+git reset --hard HEAD^1
 update_version ${SNAPSHOT}
 git add .
 git commit --message "v${SNAPSHOT} Development"
